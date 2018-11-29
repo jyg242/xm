ed92bca8325a8cfd4d55bccc9cf32b01ec179966
蒋亚光 <jiangyaguang@jiangyaguangdeMacBook-Pro.local>
2018-11-29 16:21:10 +0800

    增加备案

---
+++ src/components/beian.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>上海市松江区中心路1158号(漕河泾科技绿洲)2号楼501-1室 &nbsp;&nbsp;联系电话:17717694949</p>
+        <p>Copyright © 2018 知弗（上海）技术服务有限公司 All right reserved. 沪ICP备18029008号-1    &nbsp;&nbsp;技术支持： 小飞</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    div{
+        /* margin-top: 1rem; */
+        width: 100%;
+        height: 10rem;
+        background-color: #252527;
+        color: azure;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    p{
+        line-height: 4rem;
+    }
+</style>
