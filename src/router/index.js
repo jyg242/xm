0bf282af47a55776638784b4f7658b62c5c7182b
yangfei <yangfei@boqii.net>
2018-11-30 08:58:57 +0800

    二级目录访问

--- src/router/index.js
+++ src/router/index.js
@@ -7,24 +7,23 @@ import Login from '@/components/login'
 Vue.use(Router)
 
 export default new Router({
-    base: 'pc',
-    mode: 'history',
-    routes: [
-
-        { path: '/', name: 'Index', component: Index },
-        { path: '/login', name: 'Login', component: Login },
-        {
-            path: '/admin',
-            name: 'Admin',
-            component: Admin,
-            beforeEnter(to, from, next) {
-                if (sessionStorage.myinfo === encodeURIComponent('加油小飞')) {
-                    next()
-                } else {
-                    next('/login')
-                        // console.log(sessionStorage.admininfo)
-                }
-            }
-        }
-    ]
+	base: 'pc',
+	mode: 'history',
+	routes: [
+		{ path: '/', name: 'Index', component: Index },
+		{ path: '/login', name: 'Login', component: Login },
+		{
+			path: '/admin',
+			name: 'Admin',
+			component: Admin,
+			beforeEnter(to, from, next) {
+				if (sessionStorage.myinfo === encodeURIComponent('加油小飞')) {
+					next()
+				} else {
+					next('/login')
+					// console.log(sessionStorage.admininfo)
+				}
+			}
+		}
+	]
 })
